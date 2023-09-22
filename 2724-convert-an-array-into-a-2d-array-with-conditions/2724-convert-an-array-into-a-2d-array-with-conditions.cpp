3642eceff3a429a95f91fee9f0116e0c418c367e
Sidhant Singh <72143335+sidhant6393@users.noreply.github.com>
2023-09-23 02:13:49 +0530

    Time: 11 ms (71.55%), Space: 29.9 MB (57.13%) - LeetHub

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/2724-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        map<int,int> mp;
+        int max_rows=0;
+        for(auto i:nums)
+        {
+            mp[i]++;
+            max_rows=max(max_rows,mp[i]);
+        }
+        vector<vector<int>> v(max_rows);
+        for(auto j:mp)
+        {
+            for(int i=0;i<j.second;i++)
+            {
+                v[i].push_back(j.first);
+            }
+        }
+
+        return v;
+    }
+};
